95ac22169667dc78cb5ab426373b7964928f848e
go-sakaguchi <123789515+go-sakaguchi@users.noreply.github.com>
2023-05-19 10:02:16 +0000

    [Finish]showlink in indexpage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,29 +2,29 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-  
+
   def new
     # @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/:id'
+    redirect_to '/books/params[:id]'
   end
-  
+
   def show
-    
+    @book = Book.find(params[:id])
   end
-  
+
   def edit
-    
+
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end

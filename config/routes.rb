268f820640b758ee09577078392261f4e1b725c9
go-sakaguchi <123789515+go-sakaguchi@users.noreply.github.com>
2023-05-20 11:31:36 +0000

    [Interrupt]add destroyfunction

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ get 'books/:id' => 'books#show', as: 'book'
 post 'books' => 'books#create'
 get 'books/:id/edit' => 'books#edit', as: 'edit_book'
 patch 'books/:id' => 'books#update', as: 'update_book'
+delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end

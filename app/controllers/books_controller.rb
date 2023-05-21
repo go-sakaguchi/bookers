443f16519691fbf98fec40fb8be8b6e34643f1e9
go-sakaguchi <123789515+go-sakaguchi@users.noreply.github.com>
2023-05-21 06:00:29 +0000

    [Finish]validation of create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,8 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(book.io)
+    if book.save
+      redirect_to book_path(book.id)
     else
       redirect_to '/books'
     end
